dc0c3a624a73b8c96398e1e5ea58c74b52ec42f6
nguyentuan <dungtuann67@gmail.com>
2022-07-09 17:57:43 +0700

    update responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,24 @@ document.onscroll = function() {
     const scrollTop = window.scrollY || document.documentElement.scrollTop
     header.classList.toggle("active", scrollTop > 100)
 }
+
+// nav mobile
+const moreBtn = document.getElementById("more-btn")
+const closeBtn = document.getElementById("close-btn")
+const navMobileMenu = document.getElementById("nav-mb__menu")
+const overlay = document.querySelector(".overlay")
+
+moreBtn.onclick = () => {
+    navMobileMenu.classList.add('active')
+    overlay.style.display = 'block'
+}
+
+closeBtn.onclick = () => {
+    navMobileMenu.classList.remove('active')
+    overlay.style.display = 'none'
+}
+
+overlay.onclick = () => {
+    navMobileMenu.classList.remove('active')
+    overlay.style.display = 'none'
+}
